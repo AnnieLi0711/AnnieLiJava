77a4067728f5a675f9a72b13938ae274d30098a3
AnnieLi0711 <annie.li@smoothstack.com>
2021-07-28 11:21:19 -0700

    Push

---
+++ firstmavenproject/src/main/java/com/ss/firstmavenproject/FirstMavenClass.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.ss.firstmavenproject;
+
+/**
+ * @author Li
+ *
+ */
+public class FirstMavenClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		FirstMavenClass obj=new FirstMavenClass();
+		obj.Sample();
+
+	}
+	
+	public String Sample()
+	{
+
+		System.out.println("Hello my first beautiful maven project");
+		return "Sample";
+	}
+
+}
---
+++ firstmavenproject/src/test/java/com/ss/firstmavenproject/FirstClassTest.java
@@ -0,0 +1,14 @@
+package com.ss.firstmavenproject;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FirstClassTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
